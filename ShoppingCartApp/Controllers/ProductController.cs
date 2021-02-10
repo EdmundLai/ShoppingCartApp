24332971a76b7658a4b498bef7991d1db7679391
Edmund Lai <edmund.lai.07@gmail.com>
2021-02-09 19:36:02 -0800

    Added Edit and Delete actions to ProductController

--- ShoppingCartApp/Controllers/ProductController.cs
+++ ShoppingCartApp/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using ShoppingCartApp.Data;
 using ShoppingCartApp.Models;
@@ -39,10 +40,108 @@ namespace ShoppingCartApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Product product)
+        public async Task<IActionResult> Create(Product product)
         {
-            _shoppingAppContext.Products.Add(product);
-            _shoppingAppContext.SaveChanges();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _shoppingAppContext.Products.Add(product);
+                    await _shoppingAppContext.SaveChangesAsync();
+                }
+                catch(DbUpdateConcurrencyException)
+                {
+                    throw;
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            var categoryList = _shoppingAppContext.Categories.ToList();
+
+            ViewData["categoryList"] = categoryList;
+
+            return View(product);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _shoppingAppContext.Products.FindAsync(id);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            var categoryList = _shoppingAppContext.Categories.ToList();
+
+            ViewData["categoryList"] = categoryList;
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int? id, Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var oldProduct = await _shoppingAppContext.Products.FindAsync(id);
+
+                    oldProduct.CategoryId = product.CategoryId;
+                    oldProduct.ProductName = product.ProductName;
+                    oldProduct.ProductCost = product.ProductCost;
+
+                    _shoppingAppContext.Update(oldProduct);
+                    await _shoppingAppContext.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+
+                    throw;
+
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            var categoryList = _shoppingAppContext.Categories.ToList();
+
+            ViewData["categoryList"] = categoryList;
+
+            return View(product);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _shoppingAppContext.Products.FindAsync(id);
+
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id, bool notUsed)
+        {
+            var currProduct = await _shoppingAppContext.Products.FindAsync(id);
+            _shoppingAppContext.Remove(currProduct);
+            await _shoppingAppContext.SaveChangesAsync();
 
             return RedirectToAction("Index");
         }
